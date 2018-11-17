961f997848a991b3b468379f7d3d42919644d262
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-17 19:49:45 +0000

    updates db config

--- beno_project/settings.py
+++ beno_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'beno_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'beno_development',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
