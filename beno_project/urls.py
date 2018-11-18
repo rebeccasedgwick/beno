d70b3400b5996592d84465eb0c254bfe065c269c
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-18 12:04:37 +0000

    redirects / index to /beno/ as default

--- beno_project/urls.py
+++ beno_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('beno/', include('beno.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/beno/', permanent=True)),
 ]
