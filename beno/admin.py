cb52f5be6c78b9e344718a7fb3de32948a0e25a0
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-18 16:04:05 +0000

    Alters admin view of Task model

--- beno/admin.py
+++ beno/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from beno.models import Task, Tag
 
-admin.site.register(Task)
-admin.site.register(Tag)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('description', 'due_by', 'priority', 'complete')
+    list_filter = ('complete', 'priority', 'tag', 'due_by')
+    fieldsets = (
+        ('Task info', {
+            'fields': ('description', 'notes', 'tag')
+        }),
+        ('Prioritization', {
+            'fields': ('priority', 'due_by')
+        }),
+        ('Status', {
+            'fields': ('complete',)
+        }),
+    )
