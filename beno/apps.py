98e91d455bb0be9ae19c854c557cfc165ae1f139
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-18 12:00:06 +0000

    rename beno_posts to beno

---
+++ beno/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BenoConfig(AppConfig):
+    name = 'beno'
--- beno_project/urls.py
+++ beno_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('beno_posts/', include('beno_posts.urls')),
+    path('beno/', include('beno.urls')),
     path('admin/', admin.site.urls),
 ]
