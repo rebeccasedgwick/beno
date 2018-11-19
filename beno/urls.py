e70cafa5465d2bb220296040b485b24e738713c3
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-19 16:50:34 +0000

    Adds basic login template

--- beno/urls.py
+++ beno/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task/<int:pk>', views.TaskDetail.as_view(), name='task-detail'),
     path('tags/', views.TagList.as_view(), name='tags'),
     path('tag/<int:pk>', views.TagDetail.as_view(), name='tag-detail'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
