cb089398f5d00fad378e41af99a2388562561512
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-27 23:59:48 +0000

    correctly state db_table for new User model

--- beno/models.py
+++ beno/models.py
@@ -7,13 +7,15 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-    db_table = 'auth_user'
     first_name = CharField(_("User firstname"), blank=True, max_length=30)
     last_name = CharField(_("User lastname"), blank=True, max_length=150)
     email = CharField(_("User email"), blank=True, max_length=254)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+    class Meta:
+        db_table = 'auth_user'
 
 
 class Task(models.Model):
