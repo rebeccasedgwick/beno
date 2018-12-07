c1988b1bc4e5c25d2e5128c04bb91baf3c04def9
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-12-07 01:53:12 +0000

    Updates vue config & setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: "Hello, world"
-  }
-})
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
